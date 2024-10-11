47180326c5fda9b9fb8ea54cb4943cf55bfb8794
Ankit <211112004@stu.manit.ac.in>
2024-10-11 18:01:47 +0530

    fix(server): fix cors error

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -5,22 +5,8 @@ import { zapRouter } from "./router/zap";
 
 const app = express();
 
-
-// Define allowed origins
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://rapidflow.vercel.app'];
-
-const corsOptions = {
-    origin: function (origin: any, callback: any) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-};
-
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/zap", zapRouter);
