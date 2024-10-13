caa2c7c18edec52d54601c92e0e67c4bdd329d8c
Ankit <211112004@stu.manit.ac.in>
2024-10-13 13:13:18 +0530

    add triggerRouter and actionRouter to index file

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -2,6 +2,8 @@ import cors from "cors";
 import express from "express";
 import { userRouter } from "./router/user";
 import { zapRouter } from "./router/zap";
+import { actionRouter } from "./router/action";
+import { triggerRouter } from "./router/trigger";
 
 const app = express();
 
@@ -10,5 +12,7 @@ app.use(cors());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/zap", zapRouter);
+app.use("/api/v1/action", actionRouter);
+app.use("/api/v1/trigger", triggerRouter);
 
 app.listen(8080, () => console.log("server started"));
